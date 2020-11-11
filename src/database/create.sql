dc6708fdecb8d908f480d4f6fa9e9b98f6fd2768
Saif Ali <54573967+saifali-ch@users.noreply.github.com>
2020-11-11 20:23:30 +0500

    Removed unused libraries from project. Added SQL file to create dummy database to work with.

--- src/database/create.sql
+++ src/database/create.sql
@@ -7,7 +7,7 @@ drop table product;
 drop table category;
 create table category
 (
-    id int primary key ,
+    id   int primary key,
     name varchar2(50) unique
 );
 
@@ -15,16 +15,51 @@ create table product
 (
     id          int primary key,
     name        varchar2(50),
-    category_id int not null constraint fk_category references category(id) on delete cascade,
-    description varchar2(500) default 'Description not Provided'
+    category_id int not null
+        constraint fk_category references category (id) on delete cascade,
+    description varchar2(500) default 'Description not Provided',
+    date_added  date          default sysdate
 )
 
+Insert into category values(cat_seq.nextval, 'A-Gas');
+Insert into category values(cat_seq.nextval, 'G-Gas');
+Insert into category values(cat_seq.nextval, 'D-Gas');
+Insert into category values(cat_seq.nextval, 'S-Gas');
+Insert into category values(cat_seq.nextval, 'P-Gas');
+Insert into category values(cat_seq.nextval, 'V-Gas');
+Insert into category values(cat_seq.nextval, 'B-Gas');
+Insert into category values(cat_seq.nextval, 'F-Gas');
+Insert into category values(cat_seq.nextval, 'H-Gas');
+Insert into category values(cat_seq.nextval, 'R-Gas');
+Insert into category values(cat_seq.nextval, 'U-Gas');
+Insert into product values(pro_seq.nextval, 'Advocate', 1, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Asset', 2, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Burnout', 3, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Diesel', 4, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Direct', 5, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Flag', 6, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Gasaro', 7, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Gasoryx', 8, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Gasque', 9, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Gastastic', 10, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Harley', 11, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Hustle', 1, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Phantom', 2, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Pit', 3, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Platinum', 4, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Power', 5, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Reliable', 6, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Repair', 7, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Response', 8, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Sense', 9, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Snap', 10, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Spur', 11, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Universal', 1, 'Product desc here', sysdate);
+Insert into product values(pro_seq.nextval, 'Volt', 2, 'Product desc here', sysdate);
 
-insert into category values (cat_seq.nextval, 'Gas');
-insert into product(id, name, category_id) values (pro_seq.nextval, 'LPG10-KG', (select id from category where name='Gas'))
-
-select * from product;
-select * from category;
+select *
+from product;
+select *
+from category;
 
 commit
-
