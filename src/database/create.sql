56d3befc0a8ffe7a891a3a5ea6298ee576cf6d91
Saif Ali <54573967+saifali-ch@users.noreply.github.com>
2020-11-17 20:04:45 +0500

    Queries to create, drop Stock Table Stock Sequence.

--- src/database/create.sql
+++ src/database/create.sql
@@ -1,8 +1,11 @@
 drop sequence cat_seq;
 drop sequence pro_seq;
+drop sequence stock_seq;
 create sequence cat_seq start with 1 increment by 1 cache 100;
 create sequence pro_seq start with 1 increment by 1 cache 100;
+create sequence stock_seq start with 1 increment by 1 cache 100;
 
+drop table stock;
 drop table product;
 drop table category;
 create table category
@@ -17,9 +20,18 @@ create table product
     name        varchar2(50),
     category_id int not null
         constraint fk_category references category (id) on delete cascade,
-    description varchar2(500) default 'Description not Provided',
-    date_added  date          default sysdate
-)
+    description varchar2(500) default 'Description not Provided'
+);
+
+create table stock
+(
+    id          int primary key,
+    pro_id      int references product (id),
+    date_added  date,
+    notify_on   int,
+    total_price int,
+    quantity    int
+);
 
 Insert into category
 values (cat_seq.nextval, 'A-Gas');
@@ -44,53 +56,53 @@ values (cat_seq.nextval, 'R-Gas');
 Insert into category
 values (cat_seq.nextval, 'U-Gas');
 Insert into product
-values (pro_seq.nextval, 'Advocate', 1, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Advocate', 1, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Asset', 2, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Asset', 2, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Burnout', 3, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Burnout', 3, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Diesel', 4, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Diesel', 4, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Direct', 5, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Direct', 5, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Flag', 6, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Flag', 6, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Gasaro', 7, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Gasaro', 7, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Gasoryx', 8, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Gasoryx', 8, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Gasque', 9, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Gasque', 9, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Gastastic', 10, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Gastastic', 10, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Harley', 11, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Harley', 11, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Hustle', 1, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Hustle', 1, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Phantom', 2, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Phantom', 2, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Pit', 3, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Pit', 3, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Platinum', 4, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Platinum', 4, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Power', 5, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Power', 5, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Reliable', 6, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Reliable', 6, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Repair', 7, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Repair', 7, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Response', 8, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Response', 8, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Sense', 9, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Sense', 9, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Snap', 10, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Snap', 10, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Spur', 11, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Spur', 11, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Universal', 1, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Universal', 1, 'Product desc here');
 Insert into product
-values (pro_seq.nextval, 'Volt', 2, 'Product desc here', sysdate);
+values (pro_seq.nextval, 'Volt', 2, 'Product desc here');
 
 select *
 from product;
