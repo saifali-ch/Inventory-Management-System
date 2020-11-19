dea99df77947efbf3ce261c49dc42ac690b0e1e3
Saif Ali <54573967+saifali-ch@users.noreply.github.com>
2020-11-19 22:34:32 +0500

    Added application logo.

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.sql.SQLException;
@@ -17,12 +18,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         DBConnection.createConnection();
-        Parent root = FXMLLoader.load(getClass().getResource("/views/Menu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
 //        JFXDecorator decorator = new JFXDecorator(stage, root);
 //        decorator.setCustomMaximize(true);
         Scene scene = new Scene(root);
         stage.setTitle("Faizi Traders");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(getClass().getResource("/media/icon.png").toExternalForm()));
         stage.show();
         stage.setOnCloseRequest(e -> {
             try {
