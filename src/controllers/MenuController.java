47556b343beffa4b20b787900a1c29ef2e004775
Saif Ali <54573967+saifali-ch@users.noreply.github.com>
2020-11-30 19:40:11 +0500

    Moved pane caching logic from this class to PaneHandler class.

--- src/controllers/MenuController.java
+++ src/controllers/MenuController.java
@@ -1,41 +1,30 @@
 package controllers;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.util.HashMap;
+import util.PaneHandler;
+import util.StageHandler;
 
 
 public class MenuController {
     
-    private static final HashMap<String, AnchorPane> cachedPanes = new HashMap<>();
     public Label paneName_label;
-    @FXML
-    private AnchorPane centerPane;
+    public AnchorPane centerPane;
     
-    public void initialize() throws IOException {
-        String path = "/views/Product.fxml";
-        cachedPanes.put(path, FXMLLoader.load(getClass().getResource(path)));
-//        centerPane.getChildren().setAll(cachedPanes.get(path));
+    public void initialize() {
     }
     
-    public void setCenterPane(ActionEvent event) throws IOException {
+    public void setCenterPane(ActionEvent event) {
         Button button = (Button) event.getSource();
-        String path = button.getAccessibleText();
         paneName_label.setText(button.getText());
         
-        if (!cachedPanes.containsKey(path))
-            cachedPanes.put(path, FXMLLoader.load(getClass().getResource(path)));
-        centerPane.getChildren().setAll(cachedPanes.get(path));
+        String path = button.getAccessibleText(); // It contains the fxml path
+        centerPane.getChildren().setAll(PaneHandler.loadCachedPane(path));
         
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage stage = StageHandler.getStage(event);
         stage.titleProperty().bind(button.textProperty());
     }
 }
